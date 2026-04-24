0425afbb1aec328a7e4d991009966a90730b1dfb
vku43345 <james.vongphasouk@imail.org>
2026-04-24 12:33:14 -0600

    forgot 8080

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from mcp.server.fastmcp import FastMCP
 
 # bind host to internal network interface IP on port 8080
 ## for intermountain ists
-mcp = FastMCP("hello-server", host="0.0.0.0")
+mcp = FastMCP("hello-server", host="0.0.0.0", port=8080)
 
 @mcp.tool()
 def hello(name: str = "World") -> str:
